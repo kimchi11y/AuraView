cbeba29a60fd1c37ee0e987c826b0e03bf5e653c
Abdul Hakim Bin Abd.Razak <abdulhakim0401@gmail.com>
2026-06-24 03:13:59 +0800

    Add indicator that shows if swipe right is a like, swipe left is a nope, remove unused files

--- lib/screens/movie_swipe/discover_screen.dart
+++ lib/screens/movie_swipe/discover_screen.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
 import '../../theme/app_theme.dart';
 import '../../services/tmdb_api/tmdb_service.dart';
 import '../../services/tmdb_api/movie_model.dart';
@@ -25,7 +27,7 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
   final TmdbService _tmdbService = TmdbService();
   final SwipeService _swipeService = SwipeService();
 
-  List<Movie> _movies = [];
+  final List<Movie> _movies = [];
   bool _isLoading = true;
   int _currentPage = 1;
 
@@ -128,14 +130,6 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
           );
         }
       }
-      if (mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Liked ${movie.title}!'),
-            duration: const Duration(milliseconds: 500),
-          ),
-        );
-      }
     } else {
       if (widget.friendId != null) {
         _swipeService.swipeLeft(
@@ -208,58 +202,173 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
                       percentThresholdY,
                     ) {
                       final movie = _movies[index];
-                      return Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          image: DecorationImage(
-                            image: NetworkImage(movie.posterUrl),
-                            fit: BoxFit.cover,
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.2),
-                              blurRadius: 10,
-                              offset: const Offset(0, 5),
+                      final swipeProgress =
+                          percentThresholdX.clamp(-1.0, 1.0).toDouble();
+                      final isSwipingRight = swipeProgress > 0;
+                      final isSwipingLeft = swipeProgress < 0;
+                      final absProgress = swipeProgress.abs();
+                      final rotation = swipeProgress * 0.05;
+
+                      return Transform.rotate(
+                        angle: rotation,
+                        child: Stack(
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16),
+                                image: DecorationImage(
+                                  image: NetworkImage(movie.posterUrl),
+                                  fit: BoxFit.cover,
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color:
+                                        Colors.black.withValues(alpha: 0.2),
+                                    blurRadius: 10,
+                                    offset: const Offset(0, 5),
+                                  ),
+                                ],
+                                border: absProgress > 0.1
+                                    ? Border.all(
+                                        color: (isSwipingRight
+                                                ? const Color(0xFF22C55E)
+                                                : AppColors.error)
+                                            .withValues(alpha: absProgress),
+                                        width: 4 * absProgress,
+                                      )
+                                    : null,
+                              ),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(16),
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      Colors.transparent,
+                                      Colors.black.withValues(alpha: 0.8),
+                                    ],
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                  ),
+                                ),
+                                padding: const EdgeInsets.all(24),
+                                alignment: Alignment.bottomLeft,
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      movie.title,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      '${movie.year}  \u2022  \u2605 ${movie.rating}',
+                                      style: const TextStyle(
+                                        color: Colors.white70,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
+                            if (isSwipingRight && absProgress > 0.1)
+                              Positioned(
+                                top: 32,
+                                right: 24,
+                                child: Opacity(
+                                  opacity: absProgress,
+                                  child: Transform.scale(
+                                    scale: 0.5 + (absProgress * 1.5),
+                                    child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                        horizontal: 16,
+                                        vertical: 8,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        color: const Color(0xFF22C55E),
+                                        borderRadius:
+                                            BorderRadius.circular(8),
+                                        border: Border.all(
+                                          color: Colors.white,
+                                          width: 3,
+                                        ),
+                                      ),
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          const Icon(
+                                            Icons.favorite,
+                                            color: Colors.white,
+                                            size: 24,
+                                          ),
+                                          const SizedBox(width: 8),
+                                          Text(
+                                            'LIKE',
+                                            style: GoogleFonts.inter(
+                                              color: Colors.white,
+                                              fontSize: 28,
+                                              fontWeight: FontWeight.w800,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            if (isSwipingLeft && absProgress > 0.1)
+                              Positioned(
+                                top: 32,
+                                left: 24,
+                                child: Opacity(
+                                  opacity: absProgress,
+                                  child: Transform.scale(
+                                    scale: 0.5 + (absProgress * 1.5),
+                                    child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                        horizontal: 16,
+                                        vertical: 8,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        color: AppColors.error,
+                                        borderRadius:
+                                            BorderRadius.circular(8),
+                                        border: Border.all(
+                                          color: Colors.white,
+                                          width: 3,
+                                        ),
+                                      ),
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          const Icon(
+                                            Icons.close,
+                                            color: Colors.white,
+                                            size: 28,
+                                          ),
+                                          const SizedBox(width: 8),
+                                          Text(
+                                            'NOPE',
+                                            style: GoogleFonts.inter(
+                                              color: Colors.white,
+                                              fontSize: 28,
+                                              fontWeight: FontWeight.w800,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
                           ],
-                        ),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16),
-                            gradient: LinearGradient(
-                              colors: [
-                                Colors.transparent,
-                                Colors.black.withOpacity(0.8),
-                              ],
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                            ),
-                          ),
-                          padding: const EdgeInsets.all(24),
-                          alignment: Alignment.bottomLeft,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                movie.title,
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 8),
-                              Text(
-                                '${movie.year}  \u2022  \u2605 ${movie.rating}',
-                                style: const TextStyle(
-                                  color: Colors.white70,
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ],
-                          ),
                         ),
                       );
                     },
